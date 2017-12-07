185aadb661ee9d2b163900a30d6d58d09d3df6e3
Igor Milosavljevic <igor.milosavljevic.2644@gmail.com>
2017-12-07 13:26:07 +0100

    First working version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,50 @@
-$(document).ready(function() {
-	for(let i = 0; i < 16; i++){
-		$("#grid").append("<div class='gridElement'></div>");
-	}
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
-	$('.gridElement').hover(function(){
-		$(this).css({'background-color' : 'black',
-					 'border' : '1px solid red'});
-	});
-})
+$(document).ready(function() {
+	let gridSize = 0
+
+	$('#gridSubmit').on('click', function() {
+		$('.gridElement').remove();
+		gridSize = $("#gridNumber").val();
+
+		for(let i = 0; i < gridSize * gridSize; i++){
+		$("#grid").append("<div class='gridElement'></div>");
+		}
+
+
+		$("#black").on('click', function(){
+			$('.gridElement').hover(function() {
+			$(this).css({'background-color' : 'black',
+						 'border' : "1px solid black"})
+			});
+		});
+
+		$('#colored').on('click', function(){
+			$('.gridElement').hover(function() {
+				$(this).css({
+					'background-color' : getRandomColor()
+				});
+			});
+		});
+
+		$("#grid").css({
+			'grid-template-columns' : 'repeat(' + gridSize + ", 1fr)",
+			'grid-template-rows' : 'repeat(' + gridSize + ", 1fr)"
+			});
+		});
+
+		$("#gridReset").on('click', function(){
+			$('.gridElement').removeClass('change');
+			$('.gridElement').css({
+				"background-color" : "lightblue"
+			})
+		});
+});
+
